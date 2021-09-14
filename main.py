a6bcac94f07886ba06ad57e8df80d30864256017
ouyen <x89712@outlook.com>
2021-09-14 13:46:21 +0800

    main

--- main.py
+++ main.py
@@ -12,24 +12,26 @@ import base64
 from random import randint
 import sys
 
-ele_set={}
-captcha_code='qwqw'
-global r,js,stuid,passwd,ele_type,headless,state
+ele_set = {}
+captcha_code = 'qwqw'
+global r, js, stuid, passwd, ele_type, headless, state
+
 
 def send_message(_s):
     print(_s)
 
+
 def iaaa_login():
-    ff_op=webdriver.FirefoxOptions()
+    ff_op = webdriver.FirefoxOptions()
     if headless:
         ff_op.set_headless()
-    driver=webdriver.Firefox(options=ff_op) 
+    driver = webdriver.Firefox(options=ff_op)
     try:
         driver.get('https://elective.pku.edu.cn/')
         # iaaa login
         WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "user_name"))
-        ).send_keys(stuid)
+            EC.presence_of_element_located(
+                (By.ID, "user_name"))).send_keys(stuid)
         driver.find_element_by_id('password').send_keys(passwd)
         driver.find_element_by_id('logon_button').click()
         return driver
@@ -37,46 +39,46 @@ def iaaa_login():
         driver.quit()
         raise
 
-def single_loop(driver:webdriver.Firefox):
+
+def single_loop(driver: webdriver.Firefox):
     try:
         driver.execute_script(js)
         time.sleep(1)
-        tmp=driver.find_element_by_id('imgname')
-        img_src=tmp.get_attribute('src')
-        _begin=img_src.find('base64,')
-        im_data=(base64.b64decode(img_src[_begin+len('base64,'):]))
-        c=r.recognize(im_data)
-        print('get capture:',c.code)
-        captcha_code=c.code
+        tmp = driver.find_element_by_id('imgname')
+        img_src = tmp.get_attribute('src')
+        _begin = img_src.find('base64,')
+        im_data = (base64.b64decode(img_src[_begin + len('base64,'):]))
+        c = r.recognize(im_data)
+        print('get capture:', c.code)
+        captcha_code = c.code
     except:
         print('cap_error')
 
     # driver.find_element_by_id('validCode').send_keys(captcha_code)
-    WebDriverWait(driver,10).until(
-        EC.presence_of_element_located((By.ID,'validCode'))
-    ).send_keys(captcha_code)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.ID, 'validCode'))).send_keys(captcha_code)
 
     # tmp=driver.find_elements_by_class_name('datagrid')
-    tmp=WebDriverWait(driver,10).until(
-        EC.presence_of_all_elements_located((By.CLASS_NAME,'datagrid'))
-    )
+    tmp = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, 'datagrid')))
     # print(len(tmp))
     # print(tmp[1].text)
     # if(tmp[1].text!=)
-    even_class=tmp[0].find_elements_by_class_name('datagrid-even')
-    odd_class=tmp[0].find_elements_by_class_name('datagrid-odd')
-    class_list=even_class+odd_class
+    even_class = tmp[0].find_elements_by_class_name('datagrid-even')
+    odd_class = tmp[0].find_elements_by_class_name('datagrid-odd')
+    class_list = even_class + odd_class
     # print(len(class_list))
 
     for single_class_num in range(len(class_list)):
-        single_class=class_list[single_class_num]
-        details=single_class.find_elements_by_class_name('datagrid')
-        class_name=details[0].text+','+details[5].text
-        if(class_name in ele_set):
+        single_class = class_list[single_class_num]
+        details = single_class.find_elements_by_class_name('datagrid')
+        class_name = details[0].text + ',' + details[5].text
+        if (class_name in ele_set):
             # print(single_class_num)
-            ele_frag=[int(_) for _ in details[9].text.split(' / ')]
-            print(class_name,str(ele_frag))
-            if(ele_frag[0]>ele_frag[1]):
+            ele_frag = [int(_) for _ in details[9].text.split(' / ')]
+            print(class_name, str(ele_frag))
+            if (ele_frag[0] > ele_frag[1]):
                 details[10].click()
                 driver.switch_to.alert.accept()
                 send_message('ok')
@@ -87,52 +89,57 @@ def single_loop(driver:webdriver.Firefox):
                 # driver.switch_to.alert.accept()
                 pass
     driver.refresh()
-            
 
-if __name__=="__main__":
-    with open('config.json','r',encoding='utf-8') as f:
-        config=f.read()
-    config_json=json.loads(config)
-    stuid=config_json['stuid']
-    passwd=config_json['passwd']
-    ele_set=config_json['ele_set']
-    ele_type=int(config_json['ele_type'])
-    headless=config_json['headless']
-    state=config_json['state']
-    ele_type_list=['无双学位',"主修",'辅双']
-    state_list=['补退选',"补选"]
-    print('Read config:',stuid,passwd,str(ele_set),ele_type_list[ele_type],state_list[state],sep='\n')
-    with open('gif2base64.js','r') as f:
-        js=f.read()
+
+if __name__ == "__main__":
+    with open('config.json', 'r', encoding='utf-8') as f:
+        config = f.read()
+    config_json = json.loads(config)
+    stuid = config_json['stuid']
+    passwd = config_json['passwd']
+    ele_set = config_json['ele_set']
+    ele_type = int(config_json['ele_type'])
+    headless = config_json['headless']
+    state = config_json['state']
+    ele_type_list = ['无双学位', "主修", '辅双']
+    state_list = ['补退选', "补选"]
+    print('Read config:',
+          stuid,
+          passwd,
+          str(ele_set),
+          ele_type_list[ele_type],
+          state_list[state],
+          sep='\n')
+    with open('gif2base64.js', 'r') as f:
+        js = f.read()
     # r=CaptchaRecognizer(CNN_MODEL_FILE)
-    r=CaptchaRecognizer('./cnn.20210311.1.pt')
-    while(ele_set):
+    r = CaptchaRecognizer('./cnn.20210311.1.pt')
+    while (ele_set):
         print('Start Driver')
-        driver=iaaa_login()
+        driver = iaaa_login()
         try:
-            if ele_type:#主修
-                WebDriverWait(driver,10).until(
-                    EC.element_to_be_clickable((By.ID,'div'+str(ele_type)))
-                ).click()
+            if ele_type:  #主修
+                WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable(
+                        (By.ID, 'div' + str(ele_type)))).click()
 
-            tmp=WebDriverWait(driver,10).until(
-                EC.visibility_of_all_elements_located((By.CLASS_NAME,'titlelink1'))
-            )
+            tmp = WebDriverWait(driver, 10).until(
+                EC.visibility_of_all_elements_located(
+                    (By.CLASS_NAME, 'titlelink1')))
             # print(len(tmp))
-            tmp[3].click()#补退选
+            tmp[3].click()  #补退选
 
             print('Begin Loop')
             # begin_time=
-            loop_turn=1
-            while(ele_set and loop_turn<10):
-                print('Loop turn:',loop_turn)
+            loop_turn = 1
+            while (ele_set and loop_turn < 10):
+                print('Loop turn:', loop_turn)
                 single_loop(driver)
                 print(time.asctime())
-                time.sleep(10+randint(0,5))
-                loop_turn+=1
+                time.sleep(10 + randint(0, 5))
+                loop_turn += 1
             print('End Loop')
             driver.quit()
         except:
             print('ERROR!!!!!!!')
             driver.quit()
-
